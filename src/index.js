452f45ba759ad83b28de357b64a5c56340a49f5f
nodokaodayaka <nodokaodayaka@users.noreply.github.com>
2020-12-03 06:49:29 +0000

    Apply Prettier Change

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 const test = () => {
   console.log('test');
 
-  const a = 0 ;
-  const b = 1 ;
+  const a = 0;
+  const b = 1;
 
-  console.log(
-    a + b 
-    );
+  console.log(a + b);
 };
 
-const test2 = 1    ;
+const test2 = 1;
